3f08a391251885233240e59d827eedab0dc359b4
mfrc0085 <manuelfrancisco.rivera@grupoatu.com>
2026-04-10 17:12:03 +0000

    fix: Add missing JS and Hooks for Course Import/Export

--- lms/lms/doctype/lms_course/lms_course.js
+++ lms/lms/doctype/lms_course/lms_course.js
@@ -2,35 +2,52 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on("LMS Course", {
-	onload: function (frm) {
-		frm.set_query("chapter", "chapters", function () {
-			return {
-				filters: {
-					course: frm.doc.name,
-				},
-			};
-		});
+    onload: function (frm) {
+        frm.set_query("chapter", "chapters", function () {
+            return {
+                filters: {
+                    course: frm.doc.name,
+                },
+            };
+        });
 
-		frm.set_query("course", "related_courses", function () {
-			return {
-				filters: {
-					published: true,
-				},
-			};
-		});
-	},
-	refresh: (frm) => {
-		const lmsPath = frappe.boot.lms_path || "lms";
-		frm.add_web_link(
-			`/${lmsPath}/courses/${frm.doc.name}`,
-			"See on Website"
-		);
+        frm.set_query("course", "related_courses", function () {
+            return {
+                filters: {
+                    published: true,
+                },
+            };
+        });
+    },
 
-		if (!frm.doc.currency)
-			frappe.db
-				.get_single_value("LMS Settings", "default_currency")
-				.then((value) => {
-					frm.set_value("currency", value);
-				});
-	},
+    refresh: function (frm) {
+        const lmsPath = frappe.boot.lms_path || "lms";
+
+        frm.add_web_link(
+            `/${lmsPath}/courses/${frm.doc.name}`,
+            __("See on Website")
+        );
+
+        if (!frm.doc.currency) {
+            frappe.db
+                .get_single_value("LMS Settings", "default_currency")
+                .then((value) => {
+                    frm.set_value("currency", value);
+                });
+        }
+
+        if (!frm.is_new()) {
+            frm.add_custom_button(
+                __("Exportar Curso"),
+                function () {
+                    window.open(
+                        "/api/method/lms.lms.doctype.lms_course.lms_course.export_course"
+                        + "?course_name=" + frm.doc.name
+                    );
+                },
+                __("Actions")
+            );
+        }
+    },
 });
+// ← listview_settings y show_import_dialog eliminados: viven en course_list.js
